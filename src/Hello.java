2d887fc7f1bb37ed39500d41843899c693544dc7
skanargit <skanar@hotmail.com>
2020-06-13 15:07:15 -0400

    Initial version

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.prob1(2,3);
+
+    }
+
+    public void prob1(int i, int j) {
+        System.out.println(i + j);
+    }
+}
